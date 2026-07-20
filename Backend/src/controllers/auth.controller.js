b1ef1d0492fe309b5dd4bfdfc2e21f9c0787b741
amansingh85589 <143634983+amansingh85589@users.noreply.github.com>
2026-07-20 12:12:53 +0530

    etting cookie with production options

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -42,7 +42,12 @@ async function registerUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 24 * 60 * 60 * 1000 // 1 day
+});
 
 
     res.status(201).json({
@@ -56,7 +61,7 @@ async function registerUserController(req, res) {
 
 }
 
-
+console.log("Setting cookie with production options");
 /**
  * @name loginUserController
  * @description login a user, expects email and password in the request body
@@ -88,7 +93,12 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 24 * 60 * 60 * 1000 // 1 day
+});
     res.status(200).json({
         message: "User loggedIn successfully.",
         user: {
